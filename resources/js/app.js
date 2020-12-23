2657b733689b3252d55681c29bf1c9b039ac77fd
Foysal Rahman <foysalrahman112@gmail.com>
2020-12-24 00:27:10 +0600

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-
+import vuetify from './vuetify';
+import router from './router'
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -18,8 +19,7 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import Example from './components/ExampleComponent'; 
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +27,11 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
+ new Vue({
     el: '#app',
+    router,
+    vuetify,
+    components:{
+    'example-component': Example
+    }
 });
